e3ca005598cf99daa8a65dd2eda7e430789a4136
Kasey <46635962+kcapretta@users.noreply.github.com>
2019-10-19 14:24:30 -0700

    Create AppDelegate.swift

---
+++ AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MusicPlay
+//
+//  Created by Kassandra Capretta on 10/18/19.
+//  Copyright © 2019 Kassandra Capretta. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain internal class AppDelegate : UIResponder, UIApplicationDelegate {
+
+    internal var window: UIWindow?
+
+    internal func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]?) -> Bool
+
+    internal func applicationWillResignActive(_ application: UIApplication)
+
+    internal func applicationDidEnterBackground(_ application: UIApplication)
+
+    internal func applicationWillEnterForeground(_ application: UIApplication)
+
+    internal func applicationDidBecomeActive(_ application: UIApplication)
+
+    internal func applicationWillTerminate(_ application: UIApplication)
+}
